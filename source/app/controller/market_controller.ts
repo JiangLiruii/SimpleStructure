949ba95da2cb206e90ec6907f94ded7182ad4170
JiangLirui <535904200@qq.com>
2018-11-22 07:31:53 +0800

    update market controller

--- source/app/controller/market_controller.ts
+++ source/app/controller/market_controller.ts
@@ -1,5 +1,7 @@
-import { Controller } from "../../framework";
+import { AppEvent, Controller } from "../../framework";
 import { IController, IMediator, IModel, IView } from "../../framework/interface";
+import { NasdaqModel } from "../models/nasdaq_model";
+import { NyseModel } from "../models/nyse_model";
 import { MarketView } from "../views/market_view";
 
 class MarketController extends Controller implements IController {
@@ -10,5 +12,33 @@ class MarketController extends Controller implements IController {
     constructor(mediator:IMediator) {
         super(mediator);
         this._markeView = new MarketView(mediator);
+        this._nasdaqModel = new NasdaqModel(mediator);
+        this._nyseModel = new NyseModel(mediator);
+    }
+
+    public initialize() {
+        this.subscribeToEvents([
+            new AppEvent('app.controller.market.nasdaq', null, (e, data) => this.nasdaq(data)),
+            new AppEvent('app.controller.market.nyse', null, (e, data) => this.nyse(data)),
+        ]);
+        this._markeView.initialize();
+        this._nasdaqModel.initialize();
+        this._nyseModel.initialize();
+
+    }
+
+    public dispose() {
+        this.unsubscribeToEvents();
+        this._markeView.dispose();
+        this._nasdaqModel.dispose();
+        this._nyseModel.dispose();
+    }
+
+    private nasdaq(data) {
+        this._mediator.publish(new AppEvent('app.model.nasdaq.change', null, null));
+    }
+
+    private nyse(data) {
+        this._mediator.publish(new AppEvent('app.model.nyse.change', null, null));
     }
 }
